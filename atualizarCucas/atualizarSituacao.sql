0fc6fc758d65cb5c4ec1538cafd78ba9b55c57ca
isaiastech <isaiasbatistapy@gmail.com>
2025-01-08 15:44:45 -0300

    Atualizar situacao de produtos onde 0 desativa para venda e 1 ativa para venda

---
+++ atualizarCucas/atualizarSituacao.sql
@@ -0,0 +1,8 @@
+
+-- Atualizar Situação de produtos onde 0 desativa para venda de 1 ativa
+-- 08/01/2025
+UPDATE `sabores-cucas` SET `situacao` = '0' WHERE `sabores-cucas`.`id` = 10; 
+UPDATE `sabores-cucas` SET `situacao` = '0' WHERE `sabores-cucas`.`id` = 16; 
+UPDATE `sabores-cucas` SET `situacao` = '0' WHERE `sabores-cucas`.`id` = 17; 
+UPDATE `sabores-cucas` SET `situacao` = '0' WHERE `sabores-cucas`.`id` = 20; 
+
